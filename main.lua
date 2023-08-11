20c8b88dee1b9deeae6f7be8c5dee0e5fd391be2
BlueMods <105815028+BlueModsXL@users.noreply.github.com>
2023-08-10 21:27:43 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,11 +7,33 @@
 game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
 game.Players.LocalPlayer.Character.Humanoid.JumpSpeed = 50
 
-game:GetService("Lighting").Brightness = 2
-game:GetService("Lighting").ClockTime = 14
-game:GetService("Lighting").FogEnd = 100000
-game:GetService("Lighting").GlobalShadows = false
-game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(128, 128, 128)
+pcall(function()
+    local lighting = game:GetService("Lighting");
+    lighting.Ambient = Color3.fromRGB(255, 255, 255);
+    lighting.Brightness = 1;
+    lighting.FogEnd = 1e10;
+    for i, v in pairs(lighting:GetDescendants()) do
+        if v:IsA("BloomEffect") or v:IsA("BlurEffect") or v:IsA("ColorCorrectionEffect") or v:IsA("SunRaysEffect") then
+            v.Enabled = false;
+        end;
+    end;
+    lighting.Changed:Connect(function()
+        lighting.Ambient = Color3.fromRGB(255, 255, 255);
+        lighting.Brightness = 1;
+        lighting.FogEnd = 1e10;
+    end);
+    spawn(function()
+        local character = game:GetService("Players").LocalPlayer.Character;
+        while wait() do
+            repeat wait() until character ~= nil;
+            if not character.HumanoidRootPart:FindFirstChildWhichIsA("PointLight") then
+                local headlight = Instance.new("PointLight", character.HumanoidRootPart);
+                headlight.Brightness = 1;
+                headlight.Range = 60;
+            end;
+        end;
+    end);
+end)
 
 pcall(function()
     local lighting = game:GetService("Lighting");
